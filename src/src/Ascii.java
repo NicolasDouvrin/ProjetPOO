75a9ef6d033c31c629b5a356f63351b50aaaf0db
Unknown <samuel.drilleau@hei.yncrea.fr>
2017-12-16 21:57:20 +0100

    final final

--- src/src/Ascii.java
+++ src/src/Ascii.java
@@ -6,20 +6,20 @@ public class Ascii implements Mode {
     static String[] ascii = {"01100001","01100010","01100011","01100100","01100101","01100110","01100111","01101000","01101001","01101010","01101011","01101100","01101101","01101110","01101111","01110000","01110001","01110010","01110011","01110100","01110101","01110110","01110111","01111000","01111001","01111010","00000000"};
 
     public Ascii(String s) {
-        this.s = s;
+        this.s = s;  //constructeur
     }
 
     @Override
     public String crypt() {
         char[] charSIn = this.s.toCharArray();
-        String[] charSOut = new String[charSIn.length];
+        String[] charSOut = new String[charSIn.length]; // création d'un char de sortie pour mettre le message codé
 
         for (int i = 0; i < charSIn.length; i++) {
 
-            int j = Arrays.asList(alphabet1).indexOf(String.valueOf(charSIn[i]));
+            int j = Arrays.asList(alphabet1).indexOf(String.valueOf(charSIn[i])); //correspondance entre la lettre et son pendant en ascii
             charSOut[i] = ascii[j];
         }
-        return toString(charSOut);
+        return toString(charSOut);//retour du tableau sous forme de String
     }
 
     @Override
@@ -27,18 +27,18 @@ public class Ascii implements Mode {
         char[] charSIn = this.s.toCharArray();
         String[] charSOut = new String[(charSIn.length)/8];
 
-        for (int i=0;i<(charSIn.length)/8;i++){
+        for (int i=0;i<(charSIn.length)/8;i++){ // division du string obtenu pour faire correspondre au groupe de 8 vis à vis du code ascii
             String pate = new String();
             for (int j=0;j<8;j++){
                 pate=pate+String.valueOf(charSIn[i*8+j]);
             }
-            int k = Arrays.asList(ascii).indexOf(pate);
+            int k = Arrays.asList(ascii).indexOf(pate); //correspondance entre la lettre et son pendant en ascii
             charSOut[i] = alphabet1[k];
         }
         return toString(charSOut);
     }
 
-    private static String toString(String[] tableau){
+    private static String toString(String[] tableau){ // permet de mettre en string le tableau de char final pour l'afficher
         String texte = new String();
         for (int i=0;i<tableau.length;i++){
             texte=texte+tableau[i];
--- src/src/Cesar.java
+++ src/src/Cesar.java
@@ -13,11 +13,15 @@ public class Cesar implements Mode {
         char[] charSIn = this.s.toCharArray();
         char[] charSOut = new char[charSIn.length];
         int pos1, pos2;
-        for(int i = 0; i < charSIn.length; i++)   {
-            pos1 = posChar(charSIn[i], alphabet1);
-            pos2 = newPos(pos1, this.decal);
-            if(pos2 == -1) charSOut[i] = ' '; // si -1, c'est que ce n'est pas une lettre, on met un espace à la place
-            else charSOut[i] = alphabet1[pos2];
+        if (decal >=0) {
+            for(int i = 0; i < charSIn.length; i++)   {
+                pos1 = posChar(charSIn[i], alphabet1);
+                pos2 = newPos(pos1, this.decal);
+                if(pos2 == -1) charSOut[i] = ' '; // si -1, c'est que ce n'est pas une lettre, on met un espace à la place
+                else charSOut[i] = alphabet1[pos2];
+            }
+        }else{
+            System.out.println("Valeurs négatives non prises en charge");
         }
         return new String(charSOut); // on fait un string avec le tableau de char
     }
@@ -27,26 +31,26 @@ public class Cesar implements Mode {
         char[] charSIn = this.s.toCharArray();
         char[] charSOut = new char[charSIn.length];
         int pos1, pos2;
-        for(int i = 0; i < charSIn.length; i++) {
-            pos1 = posChar(charSIn[i], alphabet1);
-            pos2 = newPos(pos1, -(this.decal));
-            if(pos2 == -1) charSOut[i] = ' '; // si -1, c'est que ce n'est pas une lettre, on met un espace à la place
-            else charSOut[i] = alphabet1[pos2];
+        if (decal >=0) {
+            for(int i = 0; i < charSIn.length; i++) {
+                pos1 = posChar(charSIn[i], alphabet1);
+                pos2 = newPos(pos1, -(this.decal));
+                if(pos2 == -1) charSOut[i] = ' '; // si -1, c'est que ce n'est pas une lettre, on met un espace à la place
+                else charSOut[i] = alphabet1[pos2];
+            }
+        }else{
+            System.out.println("Valeurs négatives non prises en charge");
         }
         return new String(charSOut); // on fait un string avec le tableau de char
     }
 
-    // Renvoie la position du caractere dans le tableau
-    // -1 si il n'est pas dans le tableau
-
-    private static int posChar(char c, char[] tab)  {
+    private static int posChar(char c, char[] tab)  { // Renvoie la position du caractere dans le tableau ou -1 si il n'est pas dans le tableau
         for(int i = 0; i < tab.length; i++)   {
             if(tab[i] == c) return i;
         }   return -1;
     }
 
-    // Donne la nouvelle position dans l'alphabet en fonction de n
-    private static int newPos(int pos,int n)  {
+    private static int newPos(int pos,int n)  { // Donne la nouvelle position dans l'alphabet en fonction de n
         int pos2 = pos;
         if(pos <= -1) { // -1 signifie que le caractere n'a pas été trouvé dans l'alphabet (caractere spécial, chiffre, espace, etc.)
             pos2 = -1;
@@ -57,7 +61,7 @@ public class Cesar implements Mode {
                     if(pos2 - 1 == -1) pos2 = 25;
                     else pos2--;
                 } else {
-                    if(pos2 + 1 >= 25) pos2 = 0;
+                    if(pos2 + 1 >= 26) pos2 = 0;
                     else pos2++;
                 }
                 i++;
@@ -66,8 +70,7 @@ public class Cesar implements Mode {
         return pos2;
     }
 
-    // Valeur absolue de a
-    public static int abs(int a)  {
+    public static int abs(int a)  { // Valeur absolue de a
         if(a >= 0) return a;
         else return -a;
     }
